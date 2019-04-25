6562ccfb64d7166737562bb24bf547f30b520b96
Anmol Gautam <anmolgautam445@gmail.com>
2019-04-25 03:20:19 -0400

    Formatted the counter output in the dialog box

--- spimsimulator-code-r721/QtSpim/counterdialog.cpp
+++ spimsimulator-code-r721/QtSpim/counterdialog.cpp
@@ -9,11 +9,13 @@
 #include <string>
 #include <iterator>
 
+using namespace std;
 CounterDialog::CounterDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::CounterDialog)
 {
     ui->setupUi(this);
+
 
     QLabel *info_label = new QLabel(this);
     info_label->setText("The number of instructions are as follows:");
@@ -21,20 +23,19 @@ CounterDialog::CounterDialog(QWidget *parent) :
     std::map<std::string, int> instruction_count = ReturnFinalCount();
     std::map<std::string,int>::iterator it = instruction_count.begin();
 
-    int margin_value = 20;
+    //int margin_value = 20;
+    string break_line = "<br>";
     while (it != instruction_count.end()){
         QTextStream(stdout) << QString::fromStdString(it->first);
         QTextStream(stdout) << "is found "<< it->second << endl;
 
         QLabel *instruction = new QLabel(this);
-        instruction->setText(QString::fromStdString(it->first)+" : ");
-        instruction->setMargin(margin_value);
+        instruction->setText(QString::fromStdString(break_line)+QString::fromStdString(it->first)+": <br>");
         QLabel *count = new QLabel(this);
-        count->setMargin(margin_value);
         count->setIndent(80);
-        count->setNum(it->second);
+        count->setText(QString::fromStdString(break_line)+QString::fromStdString(to_string(it->second)));
+        break_line+= "<br>";
         it++;
-        margin_value += 20;
         }
 }
 
